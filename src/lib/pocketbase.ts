1cfda859c14b2a61a64bd7c56f6c3976a696e41d
Anton <antklim89@mail.ru>
2025-04-15 15:06:37 +0300

    add pocketbase type gen

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,8 +1,9 @@
 import PocketBase, { type ClientResponseError } from 'pocketbase';
 
+import type { TypedPocketBase } from '@/pocketbase-types.gen';
 import { env } from './env';
 
-export const pb = new PocketBase(env.SERVER_URL);
+export const pb = new PocketBase(env.SERVER_URL) as TypedPocketBase;
 
 export function flattenPocketbaseErrors(error: ClientResponseError) {
   return Object.entries(error.response.data).reduce((acc: Record<string, string>, [key, value]) => {
