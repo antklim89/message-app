5eb1c794d40471a41133e35e5c615aaa080812d5
Anton <antklim89@mail.ru>
2025-04-16 11:24:43 +0300

    check user in new message component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Box, Skeleton } from '@chakra-ui/react';
 import { createFileRoute } from '@tanstack/react-router';
 
 import { QuerySuspenseErrorBoundary } from '@/components/suspense/query-suspense-error-boundary';
+import { SuspenseErrorBoundary } from '@/components/suspense/suspense-error-boundary';
 import { Message, MessageList, MessageListFallback, NewMessage, useFetchManyMessages } from '@/features/messages';
+import { useUser } from '@/hooks/useUser';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -10,12 +13,21 @@ export const Route = createFileRoute('/')({
 function Index() {
   return (
     <div className="p-2">
-      <NewMessage />
+      <SuspenseErrorBoundary fallback={<Skeleton h={30} />}>
+        <NewMessageLayout />
+      </SuspenseErrorBoundary>
       <QuerySuspenseErrorBoundary fallback={<MessageListFallback />}>
         <MessageListLayout />
       </QuerySuspenseErrorBoundary>
     </div>
   );
+}
+
+function NewMessageLayout() {
+  const user = useUser();
+
+  if (user == null) return <Box h={30} />;
+  return <NewMessage />;
 }
 
 function MessageListLayout() {
