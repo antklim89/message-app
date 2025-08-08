ac39aadaeba95f5327b8fb12f49cff0b5bab8d11
Anton <antklim89@mail.ru>
2025-08-08 10:54:08 +0300

    feat: create infinity scroll hook

---
+++ src/share/hooks/use-infinity-scroll.ts
@@ -0,0 +1,43 @@
+import { type RefObject, useEffect, useRef } from 'react';
+
+const useInfiniteScroll = ({
+  loadMore,
+  rootMargin = '0px',
+  threshold = 1.0,
+  dependencies = [],
+}: {
+  loadMore?: () => Promise<unknown>;
+  dependencies?: unknown[];
+} & IntersectionObserverInit): RefObject<HTMLDivElement | null> => {
+  const sentinelRef = useRef<HTMLDivElement>(null);
+  const isLoading = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (!loadMore) return;
+    if (!sentinelRef.current) return;
+
+    const observer = new IntersectionObserver(
+      async ([entry]) => {
+        if (entry.isIntersecting && !isLoading.current) {
+          isLoading.current = true;
+          try {
+            await loadMore();
+          } finally {
+            isLoading.current = false;
+          }
+        }
+      },
+      { rootMargin, threshold },
+    );
+
+    observer.observe(sentinelRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [loadMore, rootMargin, threshold, ...dependencies]);
+
+  return sentinelRef;
+};
+
+export default useInfiniteScroll;
