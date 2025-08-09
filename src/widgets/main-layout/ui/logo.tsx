70e0c82c6103d51c2ac5fdde8860f8e52d59f220
Anton <antklim89@mail.ru>
2025-08-09 21:45:32 +0300

    fix: lint

--- src/widgets/main-layout/ui/logo.tsx
+++ src/widgets/main-layout/ui/logo.tsx
@@ -5,11 +5,11 @@ import { Link } from '@tanstack/react-router';
 import logoIcon from '@/share/assets/logo.svg';
 
 export function Logo(props: ComponentProps<'img'>) {
-    return (
-      <Flex>
-        <Link to="/">
-          <img alt="logo" height={64} src={logoIcon} width={64} {...props} />
-        </Link>
-      </Flex>
-    );
-  }
+  return (
+    <Flex>
+      <Link to="/">
+        <img alt="logo" height={64} src={logoIcon} width={64} {...props} />
+      </Link>
+    </Flex>
+  );
+}
