b603ce367b3e4300c7a3f97a4b16bbb4daaffec0
Anton <antklim89@mail.ru>
2025-08-09 20:57:46 +0300

    fix: supabase client

--- src/share/lib/supabase.ts
+++ src/share/lib/supabase.ts
@@ -1,23 +1,15 @@
-import { use } from 'react';
 import type { SupabaseClient } from '@supabase/supabase-js';
 
 import type { Database } from '@/share/model/supabase-types.generated';
 import { env } from './env';
 
 const sbPromise = import('@supabase/supabase-js');
-
 let sb: SupabaseClient<Database> | undefined;
-
-export function useSupbabase() {
-  if (sb != null) return sb;
-  const { createClient } = use(sbPromise);
-  sb = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
-  return sb;
-}
 
 export async function createSupabaseClient() {
   if (sb != null) return sb;
   const { createClient } = await sbPromise;
+  if (sb != null) return sb;
   sb = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
   return sb;
 }
