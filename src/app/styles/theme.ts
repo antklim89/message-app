c46bcf0f09a22734174885604748c6bbcdab28bd
Anton <antklim89@mail.ru>
2025-08-09 20:24:43 +0300

    feat: update theme

--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -3,30 +3,49 @@ import { createSystem, defaultConfig, defineConfig } from '@chakra-ui/react';
 const customConfig = defineConfig({
   globalCss: {
     html: {
-      colorPalette: 'teal',
+      colorPalette: 'blue',
     },
   },
   theme: {
     tokens: {
       colors: {
-        black: { value: '#232323' },
-        white: { value: '#dddddd' },
+        black: { value: '#090A0B' },
+        white: { value: '#F4F5F6' },
+        blue: {
+          '0': { value: '#92bbfe' },
+          '50': { value: '#92bbfe' },
+          '100': { value: '#68a2ff' },
+          '200': { value: '#3f89ff' },
+          '300': { value: '#1770ff' },
+          '400': { value: '#005bed' },
+          '500': { value: '#004bc4' },
+          '600': { value: '#003b9b' },
+          '700': { value: '#002c72' },
+          '800': { value: '#001c4a' },
+          '900': { value: '#000d21' },
+          '950': { value: '#000000' },
+        },
         gray: {
-          '50': { value: '#F2F2F2' },
-          '100': { value: '#DBDBDB' },
-          '200': { value: '#C4C4C4' },
-          '300': { value: '#ADADAD' },
-          '400': { value: '#969696' },
-          '500': { value: '#808080' },
-          '600': { value: '#666666' },
-          '700': { value: '#4D4D4D' },
-          '800': { value: '#333333' },
-          '900': { value: '#1A1A1A' },
+          '0': { value: '#cdd4dc' },
+          '50': { value: '#cdd4dc' },
+          '100': { value: '#b8c3cd' },
+          '200': { value: '#a3b1bf' },
+          '300': { value: '#8e9fb0' },
+          '400': { value: '#798da1' },
+          '500': { value: '#667b91' },
+          '600': { value: '#57697c' },
+          '700': { value: '#485867' },
+          '800': { value: '#394652' },
+          '900': { value: '#2b343d' },
+          '950': { value: '#1c2228' },
         },
       },
     },
     semanticTokens: {
       colors: {
+        bg: {
+          DEFAULT: { value: { _light: '{colors.white}', _dark: '{colors.black}' } },
+        },
         bgPanel: {
           DEFAULT: { value: { _light: '{colors.gray.50}', _dark: '{colors.gray.950}' } },
         },
