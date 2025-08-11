09adc4ddb20dccef413d9feea0810ae36691ed44
Anton <antklim89@mail.ru>
2025-08-11 14:40:10 +0300

    feat: create confirm dialog

--- src/entities/auth/ui/logout-button.tsx
+++ src/entities/auth/ui/logout-button.tsx
@@ -1,14 +1,22 @@
 import type { ReactNode } from 'react';
 import { Button } from '@chakra-ui/react';
 
+import { ConfirmDialog } from '@/share/ui/confirm-dialog';
 import { useLogoutMutation } from '../api/hooks/use-logout-mutation';
 
 export function LogoutButton({ children }: { children: ReactNode }) {
   const { mutateAsync: logout, isPending } = useLogoutMutation();
 
   return (
-    <Button loading={isPending} size="sm" variant="outline" onClick={() => logout()}>
-      {children}
-    </Button>
+    <ConfirmDialog
+      text="Are you sure you want to logout?"
+      trigger={
+        <Button loading={isPending} size="sm" variant="outline">
+          {children}
+        </Button>
+      }
+      onConfirm={logout}
+      isConfirming={isPending}
+    ></ConfirmDialog>
   );
 }
---
+++ src/share/ui/confirm-dialog.tsx
@@ -0,0 +1,45 @@
+import type { ReactNode } from 'react';
+import { Button, CloseButton, Dialog, Portal } from '@chakra-ui/react';
+
+export function ConfirmDialog({
+  trigger,
+  text,
+  onConfirm,
+  isConfirming,
+  confirmButtonText,
+  cancelButtonText,
+}: {
+  trigger: ReactNode;
+  text: ReactNode;
+  onConfirm: () => void;
+  isConfirming?: boolean;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+}) {
+  return (
+    <Dialog.Root placement="center" size="sm">
+      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content display="flex">
+            <Dialog.Header>
+              <Dialog.CloseTrigger asChild>
+                <CloseButton size="sm" />
+              </Dialog.CloseTrigger>
+            </Dialog.Header>
+            <Dialog.Body>{text}</Dialog.Body>
+            <Dialog.Footer>
+              <Dialog.CloseTrigger position="static" asChild>
+                <Button variant="ghost">{cancelButtonText ?? 'Cancel'}</Button>
+              </Dialog.CloseTrigger>
+              <Button loading={isConfirming} onClick={() => onConfirm()}>
+                {confirmButtonText ?? 'Confirm'}
+              </Button>
+            </Dialog.Footer>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+}
