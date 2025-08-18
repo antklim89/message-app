6675eb5ce8e9ef191b741044fa422c6e59a19ac5
Anton <antklim89@mail.ru>
2025-08-18 10:24:30 +0000

    feat: make menu items private

--- src/widgets/message-card/ui/message-card.tsx
+++ src/widgets/message-card/ui/message-card.tsx
@@ -5,30 +5,39 @@ import { Like } from '@/entities/likes';
 import { Message, type MessageType } from '@/entities/messages';
 import { MessageUpdateDialog } from '@/features/message-edit';
 import { MessageDeleteDialog } from '@/features/message-edit/ui/message-delete-dialog';
+import { Protected } from '@/share/ui/protected';
 
 export function MessageCard({ message }: { message: MessageType }) {
   return (
     <Message
       menuItems={
         <Menu.ItemGroup>
-          <MessageUpdateDialog
-            message={message}
-            trigger={
-              <Menu.Item cursor="pointer" value="update-message" asChild>
-                <Button w="full" variant="ghost">
-                  Update
-                </Button>
-              </Menu.Item>
+          <Protected
+            privateElement={
+              <MessageUpdateDialog
+                message={message}
+                trigger={
+                  <Menu.Item cursor="pointer" value="update-message" asChild>
+                    <Button w="full" variant="ghost">
+                      Update
+                    </Button>
+                  </Menu.Item>
+                }
+              />
             }
           />
-          <MessageDeleteDialog
-            id={message.id}
-            trigger={
-              <Menu.Item cursor="pointer" value="delete-message" asChild>
-                <Button w="full" variant="ghost">
-                  Delete
-                </Button>
-              </Menu.Item>
+          <Protected
+            privateElement={
+              <MessageDeleteDialog
+                id={message.id}
+                trigger={
+                  <Menu.Item cursor="pointer" value="delete-message" asChild>
+                    <Button w="full" variant="ghost">
+                      Delete
+                    </Button>
+                  </Menu.Item>
+                }
+              />
             }
           />
         </Menu.ItemGroup>
