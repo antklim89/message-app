fac257be498253a60df7e546e2753c0a445b3a6f
Anton <antklim89@mail.ru>
2025-08-21 08:14:38 +0000

    feat: add checkIsPublic

--- src/share/ui/protected.tsx
+++ src/share/ui/protected.tsx
@@ -2,19 +2,26 @@ import type { ReactNode } from 'react';
 
 import { SuspenseErrorBoundary } from './suspense-error-boundary';
 import { useSession } from '../hooks/use-session';
+import { type User } from '../model/user';
 
 export function Protected({
   fallback,
   privateElement,
   publicElement,
+  checkIsPublic,
 }: {
   publicElement?: ReactNode;
   privateElement?: ReactNode;
+  checkIsPublic?: (userId: User | null) => boolean;
   fallback?: ReactNode;
 }) {
   return (
     <SuspenseErrorBoundary fallback={fallback}>
-      <ProtectedContent privateElement={privateElement} publicElement={publicElement} />
+      <ProtectedContent
+        checkIsPublic={checkIsPublic ?? (userId => userId == null)}
+        privateElement={privateElement}
+        publicElement={publicElement}
+      />
     </SuspenseErrorBoundary>
   );
 }
@@ -22,12 +29,14 @@ export function Protected({
 export function ProtectedContent({
   privateElement,
   publicElement,
+  checkIsPublic,
 }: {
   publicElement: ReactNode | undefined;
   privateElement: ReactNode | undefined;
+  checkIsPublic: (userId: User | null) => boolean;
 }) {
   const { data: user } = useSession();
 
-  if (user == null) return publicElement;
+  if (checkIsPublic(user)) return publicElement;
   return privateElement;
 }
