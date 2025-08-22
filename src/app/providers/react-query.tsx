54285899fa37acbbcfbfdfe98c513bdf43c70cf5
Anton <antklim89@mail.ru>
2025-08-22 12:13:50 +0000

    feat: update query retry

--- src/app/providers/react-query.tsx
+++ src/app/providers/react-query.tsx
@@ -1,11 +1,20 @@
 import type { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import { ErrType, isErr } from '@/share/lib/result';
+
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnMount: true,
       refetchOnWindowFocus: false,
+      retryDelay: 1000 * 10,
+      retry(failureCount, error) {
+        if (isErr(error)) {
+          return error.type !== ErrType.NOT_FOUND;
+        }
+        return failureCount < 3;
+      },
       staleTime: 1000 * 60 * 5,
     },
   },
