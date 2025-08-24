97519232f7cb7940bb9ab6fcba379b5460ac62e3
Anton <antklim89@mail.ru>
2025-08-24 21:10:49 +0300

    feat: update auth schemas

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -1,7 +1,7 @@
 export { createUser } from './api/repository/create-user';
 export { loginWithPassword } from './api/repository/login-with-password';
 export { logout } from './api/repository/logout';
-export { BaseAuthSchema, type LoginSchema, type RegisterSchema } from './models/schemas';
+export type { LoginSchema, RegisterSchema } from './models/schemas';
 export type { AuthWithPasswordInput, CreateUserInput, UserType } from './models/types';
 export { LoginDialog } from './ui/auth-dialog';
 export { LogoutButton } from './ui/logout-button';
--- src/entities/auth/models/schemas.ts
+++ src/entities/auth/models/schemas.ts
@@ -1,22 +1,36 @@
 import { z } from 'zod/v4-mini';
 
-export const BaseAuthSchema = z.object({
-  email: z.string().check(z.email()),
-  password: z.string().check(z.minLength(1)),
-  repeat: z.string(),
-  username: z.string().check(z.minLength(2)),
-});
-
-export const RegisterSchema = BaseAuthSchema.check(
-  z.refine(data => data.password === data.repeat, {
-    message: 'Passwords do not match',
-    path: ['repeat'],
-  }),
-);
+export const RegisterSchema = z
+  .object({
+    email: z.string().check(z.email()),
+    repeat: z.string(),
+    password: z.string().check(
+      z.minLength(8, {
+        error: ({ minimum, input }) => `Field password should have ${minimum} characters. Now is ${input?.length ?? 0}`,
+      }),
+      z.regex(/^[A-Za-z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/, {
+        error: 'Password can contain only latin characters, numbers and symbols.',
+      }),
+    ),
+    username: z.string().check(
+      z.regex(/^[A-Za-z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/, {
+        error: 'Password can contain only latin characters, numbers and symbols.',
+      }),
+      z.minLength(4, {
+        error: ({ minimum, input }) => `Field password should have ${minimum} characters. Now is ${input?.length ?? 0}`,
+      }),
+    ),
+  })
+  .check(
+    z.refine(data => data.password === data.repeat, {
+      message: 'Passwords do not match',
+      path: ['repeat'],
+    }),
+  );
 
 export const LoginSchema = z.object({
-  email: BaseAuthSchema.shape.email,
-  password: BaseAuthSchema.shape.password,
+  email: RegisterSchema.shape.email,
+  password: RegisterSchema.shape.password,
 });
 
 export type RegisterSchema = z.infer<typeof RegisterSchema>;
