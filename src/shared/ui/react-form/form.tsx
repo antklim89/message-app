e6949f57d236e48fc54d800002a9abc2f4e2eaa3
Anton <antklim89@mail.ru>
2025-08-25 11:14:52 +0000

    feat: add form alert message

--- src/shared/ui/react-form/form.tsx
+++ src/shared/ui/react-form/form.tsx
@@ -1,10 +1,12 @@
 import type { ComponentProps } from 'react';
-import { Stack } from '@chakra-ui/react';
+import { Alert, Heading, Stack } from '@chakra-ui/react';
 
 import { useFormContext } from '@/shared/lib/react-form';
+import { identifyError } from '@/shared/lib/utils';
 
 export function Form({ onSubmit, children, ...props }: ComponentProps<'form'>) {
   const form = useFormContext();
+
   return (
     <Stack asChild gap={4}>
       <form
@@ -15,6 +17,25 @@ export function Form({ onSubmit, children, ...props }: ComponentProps<'form'>) {
           form.handleSubmit();
         }}
       >
+        <div>
+          <form.Subscribe selector={state => identifyError(state.errorMap.onSubmit)}>
+            {error =>
+              error != null && (
+                <Alert.Root status="error" display="flex" flexDirection="row" alignItems="center" gap={4}>
+                  <Alert.Indicator />
+                  <Alert.Content>
+                    <Alert.Title asChild>
+                      <Heading fontSize="xl" as="h4">
+                        Submitting Failed
+                      </Heading>
+                    </Alert.Title>
+                    <Alert.Description whiteSpace="pre-wrap">{error}</Alert.Description>
+                  </Alert.Content>
+                </Alert.Root>
+              )
+            }
+          </form.Subscribe>
+        </div>
         {children}
       </form>
     </Stack>
