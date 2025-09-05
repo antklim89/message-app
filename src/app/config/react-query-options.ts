baf1982baaac23aa119f91278ef80a0870a1784a
Anton <antklim89@mail.ru>
2025-09-05 09:56:54 +0000

    feat: update query options

---
+++ src/app/config/react-query-options.ts
@@ -0,0 +1,18 @@
+import { type DefaultOptions } from '@tanstack/react-query';
+
+import { ErrType, isErr } from '@/shared/lib/result';
+
+export const reactQueryOptions: DefaultOptions<Error> = {
+  queries: {
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+    retryDelay: 1000 * 10,
+    retry(failureCount, error) {
+      if (isErr(error) && error.type === ErrType.UNEXPECTED && failureCount < 3) {
+        return failureCount < 3;
+      }
+      return false;
+    },
+    staleTime: 1000 * 60 * 5,
+  },
+};
--- src/app/providers/react-query.tsx
+++ src/app/providers/react-query.tsx
@@ -1,23 +1,10 @@
 import type { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { ErrType, isErr } from '@/shared/lib/result';
+import { reactQueryOptions } from '../config/react-query-options';
 
 export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnMount: true,
-      refetchOnWindowFocus: false,
-      retryDelay: 1000 * 10,
-      retry(failureCount, error) {
-        if (isErr(error)) {
-          return error.type !== ErrType.NOT_FOUND;
-        }
-        return failureCount < 3;
-      },
-      staleTime: 1000 * 60 * 5,
-    },
-  },
+  defaultOptions: reactQueryOptions,
 });
 
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
