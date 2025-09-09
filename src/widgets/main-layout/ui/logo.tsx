f829e7b0021b9c1a64a643c9430ee1df50461a15
Anton <antklim89@mail.ru>
2025-09-09 07:50:09 +0000

    fix(styles): remove Flex

--- src/widgets/main-layout/ui/logo.tsx
+++ src/widgets/main-layout/ui/logo.tsx
@@ -1,15 +1,12 @@
 import type { ComponentProps } from 'react';
-import { Flex } from '@chakra-ui/react';
 import { Link } from '@tanstack/react-router';
 
 import logoIcon from '@/shared/assets/logo.svg';
 
 export function Logo(props: ComponentProps<'img'>) {
   return (
-    <Flex>
-      <Link to="/">
-        <img alt="logo" height={64} src={logoIcon} width={64} {...props} />
-      </Link>
-    </Flex>
+    <Link to="/">
+      <img alt="logo" height={64} src={logoIcon} width={64} {...props} />
+    </Link>
   );
 }
