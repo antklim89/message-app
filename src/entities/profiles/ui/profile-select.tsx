47b70aebd4b68974e33be6e0b588ac2b45dcf360
Anton <antklim89@mail.ru>
2025-10-07 08:10:16 +0000

    feat: update ProfileSelect
    remove input

--- src/entities/profiles/ui/profile-select.tsx
+++ src/entities/profiles/ui/profile-select.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react';
-import { Button, Input, Menu, Span, Stack, useDisclosure } from '@chakra-ui/react';
+import type { ReactElement, ReactNode } from 'react';
+import { Menu, type MenuRootProps, Span, Stack, type useDisclosure } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
-import { FaUserPlus } from 'react-icons/fa6';
 
 import { UserAvatar } from '@/shared/ui/user-avatar';
 import { getProfileListQueryOptions } from '../api/queries/use-profile-list-query';
-import { type ProfileListItemType } from '../models/types';
+import type { ProfileListItemType } from '../models/types';
 
-export function ProfileSelect({ onSelect }: { onSelect: (profile: ProfileListItemType) => void }) {
-  const disclosure = useDisclosure();
-  const [usernameTerm, setUsernameTerm] = useState('');
-  const profileListQuery = useQuery({
-    ...getProfileListQueryOptions({ search: usernameTerm }),
-    enabled: usernameTerm.length > 1,
-  });
+export function ProfileSelect({
+  onSelect,
+  value = '',
+  disclosure,
+  input,
+  trigger,
+  ...props
+}: {
+  onSelect: (profile: ProfileListItemType) => void;
+  value?: string;
+  disclosure: ReturnType<typeof useDisclosure>;
+  input?: ReactNode;
+  trigger: ReactElement;
+} & Omit<MenuRootProps, 'children' | 'onSelect'>) {
+  const profileListQuery = useQuery(getProfileListQueryOptions({ search: value }));
 
   return (
-    <Menu.Root
-      positioning={{ placement: 'bottom', strategy: 'fixed' }}
-      open={disclosure.open}
-      onOpenChange={e => disclosure.setOpen(e.open)}
-    >
-      <Menu.Trigger asChild>
-        <Button width="fit-content" size="sm">
-          <FaUserPlus />
-        </Button>
-      </Menu.Trigger>
+    <Menu.Root onSelect={() => null} open={disclosure.open} onOpenChange={e => disclosure.setOpen(e.open)} {...props}>
+      <Menu.Trigger asChild>{trigger}</Menu.Trigger>
       <Menu.Positioner>
         <Menu.Content minW={320}>
-          <Input value={usernameTerm} onChange={e => setUsernameTerm(e.target.value)} />
+          {input}
 
           {profileListQuery.data?.map(profile => (
             <Menu.Item key={profile.id} value={profile.id} onClick={() => onSelect(profile)}>
