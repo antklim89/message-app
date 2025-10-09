87726dd22c2a41f3051827724c7f90b93228f3be
Anton <antklim89@mail.ru>
2025-10-09 07:45:55 +0000

    fix: add auth check to like mutation

--- src/entities/likes/api/mutations/use-toggle-like-mutation.ts
+++ src/entities/likes/api/mutations/use-toggle-like-mutation.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { ErrType } from '@/shared/lib/result';
+import { useSession } from '@/shared/hooks/use-session';
+import { ErrType, errAuthentication } from '@/shared/lib/result';
 import { toaster } from '@/shared/lib/toaster';
 import { type MessageType, updateMessageQueryData } from '../../@x/entities/messages';
 import { addLike } from '../repository/add-like';
@@ -10,9 +11,14 @@ const TOAST_ID = 'TOGGLE_LIKES';
 
 export function useToggleLikeMutation({ messageId, hasLiked }: { messageId: MessageType['id']; hasLiked: boolean }) {
   const queryClient = useQueryClient();
+  const { user } = useSession();
 
   return useMutation({
     async mutationFn() {
+      if (!user) {
+        toaster.error({ description: 'Login to like message.', id: TOAST_ID });
+        return errAuthentication('Login to like message.');
+      }
       const result = hasLiked ? await removeLike({ messageId }) : await addLike({ messageId });
       if (result.fail && result.error.type !== ErrType.CONFLICT) return result;
       if (result.fail && result.error.type === ErrType.AUTHENTICATION) {
@@ -26,11 +32,13 @@ export function useToggleLikeMutation({ messageId, hasLiked }: { messageId: Mess
 
       return result;
     },
-    onSuccess() {
-      updateMessageQueryData({ messageId, queryClient }, prevMessage => ({
-        hasLiked: !prevMessage.hasLiked,
-        likesCount: prevMessage.hasLiked ? prevMessage.likesCount - 1 : prevMessage.likesCount + 1,
-      }));
+    onSuccess(result) {
+      if (result.success) {
+        updateMessageQueryData({ messageId, queryClient }, prevMessage => ({
+          hasLiked: !prevMessage.hasLiked,
+          likesCount: prevMessage.hasLiked ? prevMessage.likesCount - 1 : prevMessage.likesCount + 1,
+        }));
+      }
     },
   });
 }
