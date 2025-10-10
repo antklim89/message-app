6669483467f619a370e5a8e0b7bdcb3167935a37
Anton <antklim89@mail.ru>
2025-10-10 09:45:54 +0000

    feat: wrap in useCallback

--- src/features/message-edit/lib/hooks/use-rich-text-handler.ts
+++ src/features/message-edit/lib/hooks/use-rich-text-handler.ts
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import type { LexicalEditor, SerializedRootNode } from 'lexical';
 
 import { calculateLexicalTextLength } from '@/shared/lib/lexical/utils';
@@ -7,12 +7,13 @@ import { MAX_MESSAGE_BODY_LENGTH, MIN_MESSAGE_BODY_LENGTH } from '../../config/c
 export function useRichTextHandler({ onSubmit }: { onSubmit: (v?: SerializedRootNode) => Promise<void> }) {
   const ref = useRef<LexicalEditor>(null);
 
-  async function handleSubmit() {
+  const handleSubmit = useCallback(async () => {
     const value = ref.current?.getEditorState().toJSON().root;
-
     const textLength = calculateLexicalTextLength(value);
-    if (textLength <= MAX_MESSAGE_BODY_LENGTH || textLength >= MIN_MESSAGE_BODY_LENGTH) await onSubmit(value);
-  }
+    if (textLength <= MAX_MESSAGE_BODY_LENGTH || textLength >= MIN_MESSAGE_BODY_LENGTH) {
+      await onSubmit(value);
+    }
+  }, [onSubmit]);
 
   return { ref, handleSubmit };
 }
