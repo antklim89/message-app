8c47ecf9c4e764ffa60c2a6af9846f9954c0aeb5
Anton <antklim89@mail.ru>
2025-10-19 19:35:51 +0300

    feat: add checkLink

--- src/shared/model/schema-checks.ts
+++ src/shared/model/schema-checks.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod/v4-mini';
 
-export const checkLatinCharsNumberSymbols = z.regex(/^[A-Za-z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/, {
-  error: 'Password can contain only latin characters, numbers and symbols.',
-});
+export const checkLatinCharsNumberSymbols = z.regex(
+  /^[A-Za-z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/,
+  'Password can contain only latin characters, numbers and symbols.',
+);
+
+export const checkLink = z.regex(
+  /^(https?:\/\/)?(www\.)?[\w\d-]+\.[\w]{2,}(\/.*)?$/,
+  'Link should look like this https://www.example.com',
+);
