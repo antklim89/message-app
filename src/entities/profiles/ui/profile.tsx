18335180bbf4b809905d7553173bf73ed8f9ba52
Anton <antklim89@mail.ru>
2025-10-21 21:28:28 +0300

    fix(styles): update statistic

--- src/entities/profiles/ui/profile.tsx
+++ src/entities/profiles/ui/profile.tsx
@@ -1,5 +1,5 @@
-import type { ReactNode } from 'react';
-import { Badge, Card, HStack, SimpleGrid, Span, Stack, Text } from '@chakra-ui/react';
+import type { ReactElement } from 'react';
+import { Button, Card, FormatNumber, SimpleGrid, Span, Stack, Stat, Text } from '@chakra-ui/react';
 import { Link } from '@tanstack/react-router';
 
 import { FromNowDate } from '@/shared/ui/from-now-date';
@@ -7,64 +7,94 @@ import { Protected } from '@/shared/ui/protected';
 import { UserAvatar } from '@/shared/ui/user-avatar';
 import type { ProfileType } from '../models/types';
 
-export function Profile({ profile, followToggleButton }: { profile: ProfileType; followToggleButton: ReactNode }) {
+export function Profile({ profile, followToggleButton }: { profile: ProfileType; followToggleButton: ReactElement }) {
   return (
     <Card.Root border="none">
       <Card.Header asChild>
-        <HStack gap={4}>
+        <Stack gap={4} flexDirection={{ base: 'column', md: 'row' }}>
           <UserAvatar w="12rem" h="12rem" fontSize="8rem" username={profile.username} src={profile.avatar} />
-          <Stack w="full">
-            <Card.Title fontSize="4xl">{profile.username}</Card.Title>
+          <Stack w="full" gap={8}>
+            <Card.Title fontSize="4xl">
+              {profile.username}{' '}
+              <Button asChild size="xs">
+                {followToggleButton}
+              </Button>
+            </Card.Title>
             <Span color="fg/80" fontWeight="normal">
               Created: <FromNowDate fontSize="sm" date={profile.created} />
             </Span>
-            <SimpleGrid gap={2} templateColumns={{ base: '1fr 1fr', mdDown: '1fr' }}>
+            <SimpleGrid gap={2} columns={2}>
               <Protected
                 authorId={profile.id}
                 publicElement={
                   <>
-                    <Badge variant="surface" fontSize="lg" p={2}>
-                      <Span fontWeight="bold">{profile.followersCount}</Span> Followers
-                    </Badge>
-                    <Badge variant="surface" fontSize="lg" p={2}>
-                      <Span fontWeight="bold">{profile.followingsCount}</Span> Followings
-                    </Badge>
-                    <Badge cursor="pointer" variant="surface" asChild fontSize="lg" p={2}>
-                      <Link to="/profile/$profileId/messages" params={{ profileId: profile.id }}>
-                        <Span fontWeight="bold">{profile.messagesCount}</Span> Messages
-                      </Link>
-                    </Badge>
+                    <Stat.Root>
+                      <Stat.Label>Followers</Stat.Label>
+                      <Stat.ValueText>
+                        <FormatNumber value={profile.followersCount} style="decimal" />
+                      </Stat.ValueText>
+                    </Stat.Root>
+
+                    <Stat.Root>
+                      <Stat.Label>Followings</Stat.Label>
+                      <Stat.ValueText>
+                        <FormatNumber value={profile.followingsCount} style="decimal" />
+                      </Stat.ValueText>
+                    </Stat.Root>
+
+                    <Stat.Root>
+                      <Stat.Label>Messages</Stat.Label>
+                      <Stat.ValueText asChild>
+                        <Link to="/profile/$profileId/messages" params={{ profileId: profile.id }}>
+                          <FormatNumber value={profile.messagesCount} style="decimal" />
+                        </Link>
+                      </Stat.ValueText>
+                    </Stat.Root>
                   </>
                 }
                 privateElement={
                   <>
-                    <Badge cursor="pointer" variant="surface" asChild fontSize="lg" p={2}>
-                      <Link to="/followers">
-                        <Span fontWeight="bold">{profile.followersCount}</Span> Followers
-                      </Link>
-                    </Badge>
-                    <Badge cursor="pointer" variant="surface" asChild fontSize="lg" p={2}>
-                      <Link to="/followings">
-                        <Span fontWeight="bold">{profile.followingsCount}</Span> Followings
-                      </Link>
-                    </Badge>
-                    <Badge cursor="pointer" variant="surface" asChild fontSize="lg" p={2}>
-                      <Link to="/profile/$profileId/messages" params={{ profileId: profile.id }}>
-                        <Span fontWeight="bold">{profile.messagesCount}</Span> Messages
-                      </Link>
-                    </Badge>
-                    <Badge cursor="pointer" variant="surface" asChild fontSize="lg" p={2}>
-                      <Link to="/favorite-messages">
-                        <Span fontWeight="bold">{profile.favoritesCount}</Span> Favorites
-                      </Link>
-                    </Badge>
+                    <Stat.Root>
+                      <Stat.Label>Followers</Stat.Label>
+                      <Stat.ValueText asChild>
+                        <Link to="/followers">
+                          <FormatNumber value={profile.followersCount} style="decimal" />
+                        </Link>
+                      </Stat.ValueText>
+                    </Stat.Root>
+
+                    <Stat.Root>
+                      <Stat.Label>Followings</Stat.Label>
+                      <Stat.ValueText asChild>
+                        <Link to="/followings">
+                          <FormatNumber value={profile.followingsCount} style="decimal" />
+                        </Link>
+                      </Stat.ValueText>
+                    </Stat.Root>
+
+                    <Stat.Root>
+                      <Stat.Label>Messages</Stat.Label>
+                      <Stat.ValueText asChild>
+                        <Link to="/profile/$profileId/messages" params={{ profileId: profile.id }}>
+                          <FormatNumber value={profile.messagesCount} style="decimal" />
+                        </Link>
+                      </Stat.ValueText>
+                    </Stat.Root>
+
+                    <Stat.Root>
+                      <Stat.Label>Favorites</Stat.Label>
+                      <Stat.ValueText asChild>
+                        <Link to="/favorite-messages">
+                          <FormatNumber value={profile.favoritesCount} style="decimal" />
+                        </Link>
+                      </Stat.ValueText>
+                    </Stat.Root>
                   </>
                 }
               />
             </SimpleGrid>
           </Stack>
-          {followToggleButton}
-        </HStack>
+        </Stack>
       </Card.Header>
       <Card.Body>
         <Text my={8} whiteSpace="pre-wrap">
