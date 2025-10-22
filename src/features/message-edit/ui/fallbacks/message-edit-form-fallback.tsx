3e95e4107fed571aa78119823286c38b2a5723ce
Anton <antklim89@mail.ru>
2025-10-22 10:05:29 +0000

    feat: update MessageEditFormFallback

--- src/features/message-edit/ui/fallbacks/message-edit-form-fallback.tsx
+++ src/features/message-edit/ui/fallbacks/message-edit-form-fallback.tsx
@@ -1,10 +1,15 @@
-import { Box, type BoxProps, HStack, IconButton, Skeleton, Stack } from '@chakra-ui/react';
+import { type BoxProps, Button, Card, HStack, IconButton, Skeleton, SkeletonText } from '@chakra-ui/react';
 
 export const MessageEditFormFallback = (props: BoxProps) => {
   return (
-    <Box {...props}>
-      <Skeleton w="full" h={100} />
-      <Stack>
+    <Card.Root {...props}>
+      <Card.Header justifyContent="center">
+        <Card.Title textAlign="center" asChild>
+          <SkeletonText noOfLines={1}>Create Message</SkeletonText>
+        </Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Skeleton w="full" h={100} />
         <HStack mt={2}>
           {Array.from({ length: 3 }).map(() => (
             <Skeleton asChild key={Math.random()}>
@@ -19,7 +24,16 @@ export const MessageEditFormFallback = (props: BoxProps) => {
             </Skeleton>
           ))}
         </HStack>
-      </Stack>
-    </Box>
+      </Card.Body>
+
+      <Card.Footer justifyContent="flex-end">
+        <Skeleton>
+          <Button>Close</Button>
+        </Skeleton>
+        <Skeleton>
+          <Button>Update</Button>
+        </Skeleton>
+      </Card.Footer>
+    </Card.Root>
   );
 };
