68f759ba4d9e88486cbe6cd6a7dd93fa68097f29
Anton <antklim89@mail.ru>
2025-11-18 08:34:56 +0000

    fix: change MIN_MESSAGE_BODY_LENGTH to 2

--- src/features/message-edit/config/constants.ts
+++ src/features/message-edit/config/constants.ts
@@ -1,5 +1,5 @@
 export const MAX_MESSAGE_BODY_LENGTH = 600;
-export const MIN_MESSAGE_BODY_LENGTH = 5;
+export const MIN_MESSAGE_BODY_LENGTH = 2;
 
 export const MAX_FILES = 12;
 export const ACCEPT_FILES = 'image/*';
