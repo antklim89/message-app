19456f00e35d5e90d43112d08c7f73c543e67ba9
Anton <antklim89@mail.ru>
2025-11-25 12:11:36 +0000

    fix: set MAX_FILES to 4

--- src/features/message-create/config/constants.ts
+++ src/features/message-create/config/constants.ts
@@ -1,7 +1,7 @@
 export const MAX_MESSAGE_BODY_LENGTH = 600;
 export const MIN_MESSAGE_BODY_LENGTH = 2;
 
-export const MAX_FILES = 12;
+export const MAX_FILES = 4;
 export const ACCEPT_FILES = 'image/*';
 export const MESSAGE_IMAGE_MAX_WIDTH = 1280;
 export const MESSAGE_IMAGE_MAX_HEIGHT = 1024;
