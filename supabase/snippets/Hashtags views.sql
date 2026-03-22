2775f75b3de11863f91b4ae39ae876f0a4f4b429
Anton <antklim89@mail.ru>
2026-03-22 13:53:51 +0300

    feat: add limit and order

--- supabase/snippets/Hashtags views.sql
+++ supabase/snippets/Hashtags views.sql
@@ -2,26 +2,32 @@ create or replace view hashtags_month_top_view
 with
   (security_invoker = on) as
 select
-  hashtag,
+  hashtag as hashtag,
   count(*) as count
 from
   hashtags
--- where
---   date >= date_trunc('day', now() - '1 month'::interval)
+where
+  date >= date_trunc('day', now() - '1 month'::interval)
 group by
-  hashtag;
+  hashtag
+order by
+  count desc
+limit
+  20;
 
 create or replace view hashtags_week_top_view
 with
   (security_invoker = on) as
 select
-  hashtag,
+  hashtag as hashtag,
   count(*) as count
 from
   hashtags
 where
   date >= date_trunc('day', now() - '1 week'::interval)
 group by
-  hashtag;
-
--- drop VIEW hashtags_view;
+  hashtag
+order by
+  count desc
+limit
+  20;
