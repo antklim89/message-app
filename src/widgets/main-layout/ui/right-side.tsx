afa470ab8ca892bf9a370bfa57fea6219b5e70b5
Anton <antklim89@mail.ru>
2026-03-22 13:17:00 +0300

    head: add hashtag component

--- src/widgets/main-layout/ui/right-side.tsx
+++ src/widgets/main-layout/ui/right-side.tsx
@@ -1,5 +1,6 @@
 import { Stack, type StackProps } from '@chakra-ui/react';
 
+import { Hashtags } from '@/entities/hashtags';
 import { Search } from '@/features/search';
 import { ColorModeButton } from '@/shared/ui/color-mode';
 
@@ -7,6 +8,7 @@ export function RightSide(props: StackProps) {
   return (
     <Stack h="full" {...props}>
       <Search />
+      <Hashtags />
       <ColorModeButton variant="solid" mt="auto" alignSelf="flex-end" />
     </Stack>
   );
