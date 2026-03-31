d29a4b8e1be944cc0eefc0b89e2c59a11c4f5a61
Anton <antklim89@mail.ru>
2026-03-31 21:12:11 +0300

    chore: squash db

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,23 +1,21 @@
 -- inserts a row into public.profiles
-create or replace function public.handle_new_user()
-returns trigger
-language plpgsql
-security definer set search_path = ''
-as $$
+create or replace function utils.handle_new_user () returns trigger LANGUAGE plpgsql security definer
+set
+  "search_path" to '' as $$
 begin
   insert into public.profiles (id, username)
   values (
     new.id,
-    json_value(new.raw_user_meta_data, '$.username' default 'anon' on empty)
+    json_value(new.raw_user_meta_data, '$.username' default 'anon_'||gen_random_uuid() on empty)
   );
   return new;
 end;
 $$;
 
--- trigger the function every time a user is created
-create or replace trigger on_auth_user_created
-  after insert on auth.users
-  for each row execute procedure public.handle_new_user();
+create
+or replace trigger on_auth_user_created
+after INSERT on auth.users for EACH row
+execute FUNCTION utils.handle_new_user ();
 
 --
 -- buckets
