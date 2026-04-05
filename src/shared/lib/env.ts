e5646e987e0c778b6213b9931bda674a6d17c14e
Anton <antklim89@mail.ru>
2026-04-05 14:55:37 +0300

    chore: remove SERVER_URL env

--- src/shared/lib/env.ts
+++ src/shared/lib/env.ts
@@ -2,12 +2,10 @@ import { z } from 'zod/v4-mini';
 
 export const env = z
   .object({
-    SERVER_URL: z._default(z.string(), '/'),
     SUPABASE_ANON_KEY: z.string(),
     SUPABASE_URL: z.string(),
   })
   .parse({
-    SERVER_URL: import.meta.env.VITE_SERVER_URL,
     SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY,
     SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
   });
