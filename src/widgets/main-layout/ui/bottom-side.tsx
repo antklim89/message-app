ffb87fbc84ab302ed1a1cc337940529c6abc57dd
Anton <antklim89@mail.ru>
2026-04-12 15:39:33 +0300

    fix(styles): update bottom icons

--- src/widgets/main-layout/ui/bottom-side.tsx
+++ src/widgets/main-layout/ui/bottom-side.tsx
@@ -1,77 +1,78 @@
 import type { ReactNode } from 'react';
-import { Container, Flex, HStack, Icon, IconButton, Skeleton, type StackProps, useDialog } from '@chakra-ui/react';
+import { HStack, Icon, IconButton, Image, Skeleton, type StackProps, useDialog } from '@chakra-ui/react';
 import { Link } from '@tanstack/react-router';
 import { FaCircleQuestion, FaDoorOpen, FaGear, FaRegBookmark, FaUser } from 'react-icons/fa6';
 
 import { AuthDialog } from '@/features/auth';
+import logoIcon from '@/shared/assets/logo.svg';
 import { Dialog } from '@/shared/ui/dialog';
 import { Protected } from '@/shared/ui/protected';
-import { Logo } from './logo';
 
 export function BottomSide({ rightSide, ...props }: { rightSide?: ReactNode } & StackProps) {
   const dialog = useDialog();
   return (
-    <Container asChild>
-      <HStack
-        alignItems="center"
-        justifyContent="flex-end"
-        bg="bg"
-        borderColor="border"
-        borderTop="sm"
-        bottom={0}
-        gap={[2, 4]}
-        left={0}
-        position="fixed"
-        right={0}
-        {...props}
-      >
-        <Flex mr="auto" minW="6rem" justifyContent="center">
-          <Logo height={32} width={32} />
-        </Flex>
+    <HStack
+      px={4}
+      alignItems="center"
+      justifyContent="space-between"
+      bg="bg"
+      borderColor="border"
+      borderTop="sm"
+      bottom={0}
+      gap={[1, 2]}
+      left={0}
+      position="fixed"
+      right={0}
+      {...props}
+    >
+      <IconButton variant="ghost" asChild>
+        <Link to="/">
+          <Image alt="logo" src={logoIcon} width={8} />
+        </Link>
+      </IconButton>
 
-        <Protected
-          fallback={Array.from({ length: 3 }).map(() => (
-            <Skeleton key={Math.random()}>
-              <IconButton flex="0 1 8rem" />
-            </Skeleton>
-          ))}
-          privateElement={user => (
-            <>
-              <IconButton flex="0 1 8rem" aria-label="link to profile page" variant="subtle" asChild>
-                <Link to="/profile/$profileId" params={{ profileId: user.id }}>
-                  <Icon as={FaUser} />
-                </Link>
-              </IconButton>
-              <IconButton flex="0 1 8rem" aria-label="link to profile settings" variant="subtle" asChild>
-                <Link to="/profile-settings">
-                  <Icon as={FaGear} />
-                </Link>
-              </IconButton>
-              <IconButton flex="0 1 8rem" aria-label="link to favorite messages" variant="subtle" asChild>
-                <Link to="/favorite-messages">
-                  <Icon as={FaRegBookmark} />
-                </Link>
-              </IconButton>
-            </>
-          )}
-          publicElement={
-            <>
-              <AuthDialog dialog={dialog} />
-              <Dialog.Trigger dialog={dialog} flex="1 1 100%">
-                <FaDoorOpen /> Login or Register
-              </Dialog.Trigger>
-            </>
-          }
-        />
+      <Protected
+        fallback={Array.from({ length: 4 }).map(() => (
+          <Skeleton key={Math.random()}>
+            <IconButton />
+          </Skeleton>
+        ))}
+        privateElement={user => (
+          <>
+            <IconButton aria-label="link to profile page" variant="ghost" asChild>
+              <Link to="/profile/$profileId" params={{ profileId: user.id }}>
+                <Icon as={FaUser} />
+              </Link>
+            </IconButton>
+            <IconButton aria-label="link to profile settings" variant="ghost" asChild>
+              <Link to="/profile-settings">
+                <Icon as={FaGear} />
+              </Link>
+            </IconButton>
+            <IconButton aria-label="link to favorite messages" variant="ghost" asChild>
+              <Link to="/favorite-messages">
+                <Icon as={FaRegBookmark} />
+              </Link>
+            </IconButton>
+          </>
+        )}
+        publicElement={
+          <>
+            <AuthDialog dialog={dialog} />
+            <Dialog.Trigger dialog={dialog} alignSelf="flex-end">
+              <FaDoorOpen /> Login or Register
+            </Dialog.Trigger>
+          </>
+        }
+      />
 
-        <IconButton flex="0 1 8rem" aria-label="link to about page" variant="subtle" asChild>
-          <Link to="/about">
-            <Icon as={FaCircleQuestion} />
-          </Link>
-        </IconButton>
+      <IconButton aria-label="link to about page" variant="ghost" asChild>
+        <Link to="/about">
+          <Icon as={FaCircleQuestion} />
+        </Link>
+      </IconButton>
 
-        {rightSide}
-      </HStack>
-    </Container>
+      {rightSide}
+    </HStack>
   );
 }
